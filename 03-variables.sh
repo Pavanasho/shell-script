94a46562f638613091e525997507c3929a0b1061
Ashok <pavanashok5584@gmail.com>
2025-01-08 16:39:59 +0530

    Using variables code1

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ PERSON1=ram
 PERSON2=sam
 
 echo "$PERSON1:: Hi, $PERSON2"
-echo "Jyo:: Hello, ${PERSON1}"
+echo "$PERSON2:: Hello, ${PERSON1}"
 echo "$PERSON1::How are you $PERSON2?"
 echo "$PERSON2::I'm good. what about you?"
