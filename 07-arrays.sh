b6de81e5ce24bf3b6654ae0f815018001667c45a
Ashok <pavanashok5584@gmail.com>
2025-01-08 21:57:42 +0530

    Array

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Game changer" "Og" "Saalar")
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+
+echo "All movies are : ${MOVIES[@]}"
