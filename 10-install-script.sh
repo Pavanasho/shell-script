a3784892cbd1bac02073b2fa970c13762115de79
Ashok <pavanashok5584@gmail.com>
2025-01-08 22:35:56 +0530

    Mysql script

---
+++ 10-install-script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then 
+    echo "ERROR :: you must have sudo access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing mysql...failure"
+        exit 1
+    else
+        echo "Installing mysql...success"
+    fi 
+else
+    echo "mysql is already installed"    
+fi           
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing git...failure"
+        exit 1
+    else
+        echo "Installing git...success"
+    fi 
+else
+    echo "git is already installed"    
+fi           
