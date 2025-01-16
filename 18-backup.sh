7a93e78b805e3911f732382f30877903994549cd
Ashok <pavanashok5584@gmail.com>
2025-01-16 22:26:57 +0530

    Backup script for logs

---
+++ 18-backup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+USAGE() {
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
+}
+
+mkdir -p /home/ec2-user/shellscript-logs
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+if [ ! -d "$SOURCE_DIR" ]
+then
+    echo -e "$SOURCE_DIR does not exists ... please check"
+    exit 1
+fi
+
+if [ -d "$DEST_DIR" ]
+then
+    echo -e "$DEST_DIR does not exists ... please check"
+    exit 1
+fi
+
+echo "Script started execution at: $TIMESTAMP " &>> $LOG_FILE_NAME
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+if [ -n "$FILES" ]
+then
+    echo "FILES are: $FILES"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+
+    if [ -f "$ZIP_FILE" ]
+    then
+        echo -e "Successfully created zip file for files older than $DAYS"
+        while read -r filepath
+        do
+            echo "Deleting file: $filepath"
+            rm -rf $filepath
+            echo "Deleted file: $filepath"
+        done <<< $FILES
+    else
+        echo -e "$R ERROR:: $N Failed to create zip"
+        exit 1
+    fi
+else
+    echo -e "No files found older than $DAYS"
+fi                
